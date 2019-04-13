645ecdc4c4ab4fe2d0862f4b23252c7c69b0176f
Andy <kreatff@gmail.com>
2019-04-13 18:12:40 +0300

    simple spellchecker

--- src/spellchecker.hpp
+++ src/spellchecker.hpp
@@ -13,11 +13,14 @@ public:
     void load(const std::string &str) {
         std::stringstream ss;
         ss << str;
+        load(ss);
+    }
 
+    void load(std::stringstream &ss) {
         while(!ss.eof()) {
             std::string word;
             ss >> word;
-            
+
             // нужно обрезать слово от всяких ненужных символов
             word = trim(word, " ,:\"'!?@#%$^&*()");
 
@@ -27,6 +30,35 @@ public:
     }
 
     // проверка текста
+    std::string check(std::string &str) {
+        std::stringstream ss;
+        ss << str;
+        return check(ss);
+    }
+
+    std::string check(std::stringstream &ss) {
+        std::stringstream rs;
+        while(!ss.eof()) {
+            std::string word;
+            ss >> word;
+
+            // нужно обрезать слово от всяких ненужных символов
+            word = trim(word, " ,:\"'!?@#%$^&*()");
+            word = to_lower(word);
+            
+            // 
+            auto results = dic_.lookup(word, 1);
+            std::sort(std::begin(results), std::end(results), [] (const auto &l, const auto &r) { return l.first < r.first; });
+            if(!results.empty()) {
+                rs << results.front().second << " ";
+            } else {
+                rs << word;
+            }
+        }
+
+        return rs.str();
+    }
+
 
 protected:
     Spellchecker(Spellchecker &&) = delete;
--- test/spellchecker_test.cpp
+++ test/spellchecker_test.cpp
@@ -2,5 +2,14 @@
 #include "../src/spellchecker.hpp"
 
 TEST(spellchecker, simpleTest) {
-   
+    std::string dic = "hello world how are you";
+
+    std::string str = "Hello wold! Cow ArE you?";
+    std::string eta = "Hello world! how are you?";
+
+    spellchecker::Spellchecker sc;
+    sc.load(dic);
+
+    auto r = sc.check(str);
+    std::cout << r << std::endl;
 }
